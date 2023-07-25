f93f563fe4a1f19014940033d3e091f8d9ae2f79
DianaYepes05 <DianaYepes05@users.noreply.github.com>
2023-07-25 09:29:37 +0200

    added axios and api to JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+function search(response) {
+  console.log(response.data);
+}
+
 let apiKey = "2e036aa0bt1df0677b37040f98ffo9f4";
-let currentUrl =
-  "https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}&units=metric";
+let units = "metric";
+let city = "Madrid";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(search);
