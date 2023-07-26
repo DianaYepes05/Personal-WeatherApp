7fedd77569759fd84ff65ce20e8e7e1b12e11c81
DianaYepes05 <DianaYepes05@users.noreply.github.com>
2023-07-26 23:36:34 +0200

    add even listener and made functional the search

--- src/index.js
+++ src/index.js
@@ -25,15 +25,15 @@ function todayIs() {
     "November",
     "December",
   ];
-
+  
   let day = days[now.getDay()];
-
+  
   let date = now.getDate();
-
+  
   let month = months[now.getMonth()];
-
+  
   let today = `${day}, ${month} ${date}`;
-
+  
   return today;
 }
 
@@ -44,20 +44,30 @@ function currentTime() {
   if (hour < 10) {
     hour = `0${hour}`;
   }
-
+  
   let minutes = now.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-
+  
   let timing = `${hour}:${minutes}`;
   return timing;
 }
 
 document.querySelector("#time").innerHTML = currentTime();
 
-function search(response) {
+function search(city){
+  let apiKey = "6643c7326a4c2a38838264a28531d97e";
+  let units = "metric";
+ // let city = "Madrid";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(temperatureInfo);
+}
+
+function temperatureInfo(response) {
   console.log(response.data);
+
+  document.querySelector("#city").innerHTML = response.data.name;
 
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
@@ -68,13 +78,40 @@ function search(response) {
   document.querySelector("#lowTemp").innerHTML = Math.round(
     response.data.main.temp_min
   );
+  document.querySelector("#humidity").innerHTML = Math.round(
+    response.data.main.humidity
+  );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
 }
 
-let apiKey = "6643c7326a4c2a38838264a28531d97e";
-let units = "metric";
-let city = "Madrid";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+function cityName(event) {
+  event.preventDefault();
 
-axios.get(apiUrl).then(search);
+  document.querySelector("#city").innerHTML = newCity.value;
 
-// search("Madrid");
+  let city = newCity.value;
+  search(city);
+}
+
+let citySearch = document.querySelector("#searchingRow");
+citySearch.addEventListener("click", cityName);
+
+function currentLocation(position) {
+  let units = "metric";
+  let apiKey = "6643c7326a4c2a38838264a28531d97e";
+  let weatherInfo = "https://api.openweathermap.org/data/2.5/weather?";
+  let apiUrl = `${weatherInfo}lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(temperatureInfo);
+}
+
+function currentButtonWeather() {
+  navigator.geolocation.getCurrentPosition(currentLocation);
+}
+
+let currentButton = document.querySelector("#currentButton");
+currentButton.addEventListener("click", currentButtonWeather);
+
+search("New York");
